5455532c2fba605859a021d5235e3c29139578a3
Yu Hong Fan <yuhong_fan_01@hotmail.com>
2024-05-20 17:26:32 -0400

    Implete complete layout.

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import CountUp from 'react-countup';
+
+const AnimatedCounter = ( {amount}: {amount: number}) => {
+  return (
+    
+        <CountUp 
+           duration={4}
+           decimals={2}
+           decimal=","
+           prefix="$"
+           end={amount} />
+    
+  )
+}
+
+export default AnimatedCounter
